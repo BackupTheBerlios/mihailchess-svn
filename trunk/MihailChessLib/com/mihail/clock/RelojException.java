9c2dfe79a4670bb76dff125f4d499ad99d8953e6
quik_ <quik_@b74b199d-bcfc-0310-b8b6-c4aa47745ea0>
2005-07-27 00:57:35 +0000

    Anhadidos RelojException y RelojTest al svn.

---
+++ trunk/MihailChessLib/com/mihail/clock/RelojException.java
@@ -0,0 +1,24 @@
+package com.mihail.clock;
+
+/**
+ * Esta clase define una RelojException, que son las excepciones que se lanzan
+ * cuando hay algun problema con los relojes.
+ * 
+ * @author Pedro Suarez Casal
+ * @author Iago Porto Diaz
+ *  
+ */
+public class RelojException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+     * Construye una RelojException con el mensaje de detalle especificado.
+     * 
+     * @param s
+     *            Mensaje que detalla la excepcion.
+     */
+    public RelojException(String s) {
+        super(s);
+    }
+}
---
+++ trunk/MihailChessLib/com/mihail/test/RelojTest.java
@@ -0,0 +1,34 @@
+package com.mihail.test;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import com.mihail.clock.Reloj;
+import com.mihail.clock.RelojException;
+
+public class RelojTest implements Observer {
+
+	public void update(Observable arg0, Object arg1) {
+		if (arg0 instanceof Reloj) {
+			Reloj r = (Reloj) arg0;
+			System.out.println(r.getHoras() + ":" + r.getMinutos() + ":"
+					+ r.getSegundos() + "." + r.getDecimas());
+			if (r.isFinTiempo())
+				System.out.println("FIN TIEMPO");
+		}
+	}
+
+	public static void main(String args[]) {
+		Reloj reloj = null;
+		RelojTest relojTest = null;
+		try {
+			reloj = new Reloj(0, 0, 10);
+		} catch (RelojException e) {
+		}
+		relojTest = new RelojTest();
+
+		reloj.addObserver(relojTest);
+		reloj.comenzar();
+		reloj.setParado(false);
+	}
+}
