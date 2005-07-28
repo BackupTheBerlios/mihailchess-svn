2f100d3a883e3442a16d5823b4ae0c04cdb6602f
gauleng <gauleng@b74b199d-bcfc-0310-b8b6-c4aa47745ea0>
2005-07-28 14:36:43 +0000

    Establecido Item como clase privada de DiccionarioPosiciones

--- trunk/MihailChessLib/com/mihail/chess/DiccionarioPosiciones.java
+++ trunk/MihailChessLib/com/mihail/chess/DiccionarioPosiciones.java
@@ -93,33 +93,33 @@ public final class DiccionarioPosiciones {
 		cad += "]";
 		return cad;
 	}
-}
+	
+	private final class Item {
+		private int clave;
 
-final class Item {
-	private int clave;
+		private int repeticiones = 1;
 
-	private int repeticiones = 1;
+		public Item(int num) {
+			clave = num;
+		}
 
-	public Item(int num) {
-		clave = num;
+		public int getClave() {
+			return clave;
+		}
+
+		public int getRepeticiones() {
+			return repeticiones;
+		}
+
+		public void aumentarRep() {
+			repeticiones++;
+		}
+
+		public String toString() {
+			String cad = "(" + clave + ", " + repeticiones + ")";
+
+			return cad;
+		}
+
 	}
-
-	public int getClave() {
-		return clave;
-	}
-
-	public int getRepeticiones() {
-		return repeticiones;
-	}
-
-	public void aumentarRep() {
-		repeticiones++;
-	}
-
-	public String toString() {
-		String cad = "(" + clave + ", " + repeticiones + ")";
-
-		return cad;
-	}
-
 }
